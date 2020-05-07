f62b5324874105ebff81990e2b1af2ba9c6794ad
DanilAndreev <33941552+DanilAndreev@users.noreply.github.com>
2020-05-07 19:54:44 +0300

    mui creating layout

--- starscream_web/src/App.js
+++ starscream_web/src/App.js
@@ -2,23 +2,20 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import Grid from "@material-ui/core/Grid";
+
+import ThreadsViewer from "./Pages/ThreadsViewer/ThreadsViewer";
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <Grid container>
+            <Grid xs={0} md={2} />
+            <Grid xs={12} md={8} id={'page'}>
+                <ThreadsViewer />
+            </Grid>
+            <Grid xs={0} md={2} />
+        </Grid>
     </div>
   );
 }
