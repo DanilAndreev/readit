0988d753391513fd624c97d370a80f26631f474d
CyanoFresh <cyanofresh@gmail.com>
2020-05-17 22:11:33 +0300

    cors fix

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://localhost:3000'],
+    'allowed_origins' => ['http://95.164.10.203:3000/'],
 
     'allowed_origins_patterns' => [],
 
