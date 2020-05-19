eac229db6b8f174494ba7406a7b0812cbfa1987f
CyanoFresh <cyanofresh@gmail.com>
2020-05-19 20:30:26 +0300

    cors in env

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://95.164.10.203:3000/'],
+    'allowed_origins' => [env('CORS_ORIGIN', '*')],
 
     'allowed_origins_patterns' => [],
 
