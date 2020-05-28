296676b96414ad05fd4fa40271d30dd623199a09
DanilAndreev <33941552+DanilAndreev@users.noreply.github.com>
2020-05-28 09:49:59 +0300

    added new stable build, added favicon and changed title

--- web/build/service-worker.js
+++ web/build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "./precache-manifest.e380c184984cbe0c4153614a81403193.js"
+  "https://art.apeps.pp.ua/precache-manifest.3aba99f1544c610a0ea1c2cacfab50c6.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("./index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("https://art.apeps.pp.ua/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
